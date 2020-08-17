df46b8a1ca090f325350e275f2bf55d86e119b6d
laurenmaher13@hotmail.com <laurenmaher13@hotmail.com>
2020-08-18 00:40:01 +0100

    edits

---
+++ Assets/Scripts/AnimationScripts/GranTalk.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranTalk : MonoBehaviour {
+
+    Animator anim;
+   
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void OnMouseDown()
+    {
+   
+        {
+            anim.SetBool("Sitting Talking", true);
+        
+        }
+     
+    }
+}
+
+
